1ed02f742b1837d4301c837d7569601a31efcaed
Faraan <faqfaraan@gmail.com>
2018-05-06 10:25:58 -0400

    complete project except LS

--- src/app/services/log.service.ts
+++ src/app/services/log.service.ts
@@ -12,12 +12,17 @@ export class LogService {
   private logSource = new BehaviorSubject<Log>({id: null, text: null, date: null});
   selectedLog = this.logSource.asObservable();
 
+  private stateSource = new BehaviorSubject<boolean>(true);
+  stateClear = this.stateSource.asObservable();
+
   constructor() {
-  this.logs = [
-    {id: '1', text: 'Generated components', date: new Date('12/26/2017 12:54:23')},
-    {id: '2', text: 'Added bootstrap', date: new Date('12/27/2017 9:33:33')},
-    {id: '3', text: 'Added logs component', date: new Date('12/28/2017 12:00:04')}
-    ]
+  // this.logs = [
+  //   {id: '1', text: 'Generated components', date: new Date('12/26/2017 12:54:23')},
+  //   {id: '2', text: 'Added bootstrap', date: new Date('12/27/2017 9:33:33')},
+  //   {id: '3', text: 'Added logs component', date: new Date('12/28/2017 12:00:04')}
+  //   ]
+
+    this.logs = [];
   }
 
   getLogs(): Observable<Log[]> {
@@ -26,6 +31,31 @@ export class LogService {
 
   setFormLog(log: Log) {
     this.logSource.next(log);
+  }
+
+  addLog(log: Log) {
+    this.logs.unshift(log);
+  }
+
+  updateLog(log: Log) {
+    this.logs.forEach((cur, index) => {
+      if(log.id === cur.id) {
+        this.logs.splice(index, 1);
+      }
+    });
+    this.logs.unshift(log);
+  }
+
+  deleteLog(log: Log) {
+    this.logs.forEach((cur, index) => {
+      if(log.id === cur.id) {
+        this.logs.splice(index, 1);
+      }
+    });
+  }
+
+  clearState() {
+    this.stateSource.next(true);
   }
 }
 
