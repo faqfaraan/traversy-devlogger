604a75b40b4f7187d3dfc220429b4296afb13e5d
Faraan <faqfaraan@gmail.com>
2018-05-05 23:02:18 -0400

    first commit

---
+++ src/app/components/logs/logs.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Log } from '../../models/Log';
+
+import { LogService } from '../../services/log.service';
+
+@Component({
+  selector: 'app-logs',
+  templateUrl: './logs.component.html',
+  styleUrls: ['./logs.component.css']
+})
+export class LogsComponent implements OnInit {
+  logs: Log[];
+
+  constructor(private logService: LogService) { }
+
+  ngOnInit() {
+    this.logService.getLogs().subscribe(logs => {
+      this.logs = logs;
+    });
+  }
+
+  onSelect(log: Log) {
+    this.logService.setFormLog(log);
+  }
+
+}
